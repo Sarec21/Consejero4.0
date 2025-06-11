309efcb09da2c425a27188f34e200d380501fab8
Sarec21 <129178034+Sarec21@users.noreply.github.com>
2025-06-11 22:30:43 +0200

    Add mutation system and sample mutations

--- src/screens/logic/TurnScreen.tsx
+++ src/screens/logic/TurnScreen.tsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useGameState } from '../../state/gameState'
+import { checkAndTriggerMutations } from '../../lib/mutationLogic'
 import ViewTurnScreen from '../view/ViewTurnScreen'
 
 export default function TurnScreen() {
-  const { setPlayerAdvice } = useGameState()
+  const {
+    setPlayerAdvice,
+    mainPlot,
+    currentTurn,
+    setCurrentTurn,
+  } = useGameState()
   const [advice, setAdvice] = useState('')
   const navigate = useNavigate()
 
   const handleSend = () => {
     setPlayerAdvice(advice)
+    const newTurn = currentTurn + 1
+    setCurrentTurn(newTurn)
+    checkAndTriggerMutations(newTurn, mainPlot, useGameState.getState())
     navigate('/reaction')
   }
 
