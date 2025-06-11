8f13c7920f49078311c15fbc37178260d5f811eb
Sarec21 <129178034+Sarec21@users.noreply.github.com>
2025-06-11 16:53:25 +0200

    Add mock content and state logic to game screens

--- src/screens/FinalScreen.tsx
+++ src/screens/FinalScreen.tsx
@@ -1,12 +1,23 @@
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { useGameState } from '../state/gameState'
 
 export default function FinalScreen() {
+  const navigate = useNavigate()
+  const { setKingName, setKingdom, setPlayerAdvice, setKingReaction } =
+    useGameState()
+
+  const handleReset = () => {
+    setKingName('')
+    setKingdom('')
+    setPlayerAdvice('')
+    setKingReaction('')
+    navigate('/')
+  }
+
   return (
     <div>
-      <h2>Thank you for playing!</h2>
-      <Link to="/">
-        <button>Back to Start</button>
-      </Link>
+      <p>Your service has ended. The future of Eldoria is uncertain.</p>
+      <button onClick={handleReset}>Play Again</button>
     </div>
   )
 }
--- src/screens/ReactionScreen.tsx
+++ src/screens/ReactionScreen.tsx
@@ -1,10 +1,17 @@
 import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
 import { useGameState } from '../state/gameState'
 
 export default function ReactionScreen() {
-  const { kingReaction } = useGameState()
+  const { kingName, playerAdvice, kingReaction, setKingReaction } = useGameState()
+
+  useEffect(() => {
+    setKingReaction('The King nods solemnly, but his gaze is stern.')
+  }, [setKingReaction])
+
   return (
     <div>
+      <p>Your advice to King {kingName}: {playerAdvice}</p>
       <p>{kingReaction}</p>
       <Link to="/final">
         <button>End Game</button>
